1d79fa1da40bb01caec756e32716a739debb1ac1
Erik Michaelson <erik.michaelson@gmail.com>
2020-09-08 13:20:24 -0400

    add vscode debugging

--- dist/ui/components/Job.d.ts
+++ dist/ui/components/Job.d.ts
@@ -2,7 +2,7 @@
 import { AppJob } from '../../@types/app';
 export declare const Job: ({ job, status, queueName, retryJob, promoteJob, }: {
     job: AppJob;
-    status: "latest" | "active" | "waiting" | "completed" | "failed" | "delayed" | "paused";
+    status: "active" | "failed" | "waiting" | "paused" | "completed" | "delayed" | "latest";
     queueName: string;
     retryJob: (job: AppJob) => () => Promise<void>;
     promoteJob: (job: AppJob) => () => Promise<void>;
